ef653807cdb68ad2bce8fd9a27e5b29fd287edf1
Brian Thorne <bthornemail@gmail.com>
2025-10-03 09:04:11 -0700

    hgnn consildiation

--- project/scripts/obsidian-uri.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp ./obsidian-uri.desktop ~/.local/share/applications/obsidian-uri.desktop
-xdg-mime default obsidian-uri.desktop x-scheme-handler/obsidian
--- project/scripts/redis.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ssh -L 6379:127.0.0.1:6379 main@life2d.com &
